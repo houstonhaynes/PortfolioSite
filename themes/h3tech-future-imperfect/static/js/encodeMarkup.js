5fc05651a3b41f4106d510c57ea0f3fc2057408b
houstonhaynes <h3techsme@gmail.com>
2020-07-20 09:31:42 -0700

    update code pre and highcharts to 538

--- themes/h3tech-future-imperfect/static/js/encodeMarkup.js
+++ themes/h3tech-future-imperfect/static/js/encodeMarkup.js
@@ -1,3 +1,3 @@
-document.querySelectorAll("code").forEach(function(element) {
+document.querySelectorAll("code:not(.r):not(.java):not(.groovy):not(.sql)").forEach(function(element) {
     element.innerHTML = element.innerHTML.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
 });
