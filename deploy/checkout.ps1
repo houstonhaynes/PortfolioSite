0a83b4ad58d6ef0eb546179808edc386e6c8dc17
houstonhaynes <h3techsme@gmail.com>
2020-10-30 13:09:28 -0700

    updated deploy process - created sidebar

--- deploy/checkout.ps1
+++ deploy/checkout.ps1
@@ -2,4 +2,3 @@ Set-Location -Path C:\repo\PortfolioSite
 
 git checkout main
 git pull
-4561
--- deploy/push.ps1
+++ deploy/push.ps1
@@ -1,26 +1,40 @@
-$environment = $args[0]
+Param
+    (
+        # environment help description
+        [Parameter(Mandatory=$true)]   
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]        
+        [string]$environment = $args[0]
+    )
 
+# get connection strings from local file
 $JSON = Get-Content -Path "C:\repo\PortfolioSite\deploy\properties.json" | ConvertFrom-JSON
 
+# populate connection strings for each environment
 $intString = $JSON.int
 $prdString = $JSON.prd
 
+# clear the files to ensure that a 'clean' build is generated for all files
 Remove-Item C:\repo\PortfolioSite\public\* -Recurse -Force
 
+# run R terminal to build blogdown site for specific URL pattern
 If ($environment -eq 'prd') {
-    cmd.exe /C '"C:\Program Files\R\R-4.0.2\bin\x64\Rscript.exe" build-prd.R'
+    cmd.exe /C '"C:\Program Files\Microsoft\R Open\R-4.0.2\bin\x64\Rscript.exe" build-prd.R'
 } else {
-    cmd.exe /C '"C:\Program Files\R\R-4.0.2\bin\x64\Rscript.exe" build-int.R'
+    cmd.exe /C '"C:\Program Files\Microsoft\R Open\R-4.0.2\bin\x64\Rscript.exe" build-int.R'
 }
 
+# iframe embed bug the widgetframe R package - this is the workaround
 Get-ChildItem  -Path c:\repo\PortfolioSite\public\*.html -recurse | ForEach { If (Get-Content $_.FullName | Select-String -Pattern '/figure-html//widgets/') 
            {(Get-Content $_ | ForEach {$_ -replace '/figure-html//widgets/', '/figure-html/widgets/'}) | Set-Content $_ }
            }
 
+# do the file push using azcopy sync function
 If ($environment -eq 'prd') {
     az storage blob sync -c '$web' -s "C:\repo\PortfolioSite\public" --connection-string $prdString
 } else {
     az storage blob sync -c '$web' -s "C:\repo\PortfolioSite\public" --connection-string $intString
 }
 
+# invalidate the CDN cache so that new files show on the site
 az cdn endpoint purge -g h3tech -n h3techdevCDN$environment --profile-name h3tech --content-paths '/*'
