652527afb855481335d35a9c241b64a017080f78
Houston Haynes <houston.h@mydailylive.com>
2021-02-22 19:44:04 -0800

    before fixing deploy issue

--- deploy/push.ps1
+++ deploy/push.ps1
@@ -8,15 +8,15 @@ $prdString = $JSON.prd
 Remove-Item C:\repo\PortfolioSite\public\* -Recurse -Force
 
 If ($environment -eq 'prd') {
-    cmd.exe /C '"C:\Program Files\R\R-4.0.2\bin\x64\Rscript.exe" build-prd.R'
+    cmd.exe /C '"C:\Program Files\Microsoft\R Open\R-4.0.2\bin\x64\Rscript.exe" build-prd.R'
 } else {
-    cmd.exe /C '"C:\Program Files\R\R-4.0.2\bin\x64\Rscript.exe" build-int.R'
+    cmd.exe /C '"C:\Program Files\Microsoft\R Open\R-4.0.2\bin\x64\Rscript.exe" build-int.R'
 }
 
 Get-ChildItem  -Path c:\repo\PortfolioSite\public\*.html -recurse | ForEach { If (Get-Content $_.FullName | Select-String -Pattern '/figure-html//widgets/') 
            {(Get-Content $_ | ForEach {$_ -replace '/figure-html//widgets/', '/figure-html/widgets/'}) | Set-Content $_ }
            }
-
+<#
 If ($environment -eq 'prd') {
     az storage blob sync -c '$web' -s "C:\repo\PortfolioSite\public" --connection-string $prdString
 } else {
@@ -24,3 +24,5 @@ If ($environment -eq 'prd') {
 }
 
 az cdn endpoint purge -g h3tech -n h3techdevCDN$environment --profile-name h3tech --content-paths '/*'
+
+#>
